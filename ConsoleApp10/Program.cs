5ae25a1f30120201b76e90b6bf5975a502c5e5b6
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-02-11 10:28:36 +0400

    Сссылка

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -7,8 +7,12 @@ namespace ConsoleApp10
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
-                '
+
+
+
+
+        https://prod.liveshare.vsengsaas.visualstudio.com/join?7E305E672260EEF1660A9DFF4CC4DE15D3EB
+
         }
     }
 }
