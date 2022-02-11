ea1c7e7430efb0784530c4c1e1448fbad54f18ae
stud <stud@DESKTOP-07NB5T8>
2022-02-11 10:13:31 +0400

    комит9

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -7,9 +7,14 @@ namespace ConsoleApp10
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int a = 1000;
+            while (a > 1)
+            {
+                a = a - 7;
+                Console.WriteLine(a);
+            }
+            Console.ReadLine();
             
-                
-
 
 
 
