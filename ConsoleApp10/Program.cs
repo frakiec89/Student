dfa015a7c11788c592050476d3ffe2de6c3191ec
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-02-11 09:40:45 +0400

    Комит 4

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -7,12 +7,7 @@ namespace ConsoleApp10
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            ///
-            ///
-            ///
-            '[glf[g
-                ;pgkfpgf
-                dfmdf
+            
                 '
         }
     }
