b215a62d0a3a6988d8e72bddae48d8ae07b200a1
Zapyataya13 <63292225+Zapyataya13@users.noreply.github.com>
2022-02-11 10:04:02 +0400

    Коммит7

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -7,8 +7,7 @@ namespace ConsoleApp10
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
-                
+<<<<<<< HEAD
 
 
 
@@ -16,9 +15,22 @@ namespace ConsoleApp10
 
 
 
+=======
+>>>>>>> a61252c6d90f81d8402c7219acc0368122bbaf44
+
+            int a = 1000;
+            Console.WriteLine("эй богдан");
+            while (a > 0)
+            {
+                Console.WriteLine($" {a} - 7 ");
+            }
 
 
-                //  Привет   это  первый совместный  коммит 
+
+            //  Привет   это  первый совместный  коммит 
+
+            Console.WriteLine("я канеки кен 1000-7. Нет ты дурак");
+
         }
     }
 }
